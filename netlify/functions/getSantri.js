a7911f73b6c182cb8b2a6aa4b0a3f63ce4a1fc24
digitalmtq <digitalmtq@gmail.com>
2025-08-19 21:01:31 +0700

    Update getSantri.js

--- netlify/functions/getSantri.js
+++ netlify/functions/getSantri.js
@@ -10,7 +10,7 @@ export async function handler(event) {
   }
 
   // URL API GitHub (bukan raw)
-  const apiUrl = `https://api.github.com/repos/dickymiswardi/private/contents/${kelas}.json`;
+  const apiUrl = `https://api.github.com/repos/digitalmtq/server/contents/${kelas}.json`;
 
   try {
     const response = await fetch(apiUrl, {
